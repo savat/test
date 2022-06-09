b4a49fcc508590bba6f4d0bd491f27f4b2fc26e4
savat <savat1619@gmail.com>
2022-06-09 15:54:19 +0700

    Update Ssa.sh

--- Ssa.sh
+++ Ssa.sh
@@ -1,1 +1,1 @@
-pkg up -y && pkg install proot-distro -y && proot-distro install debian-buster && proot-distro login debian-buster
+apt-get update && apt-get upgrade -y && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git nano -y && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
